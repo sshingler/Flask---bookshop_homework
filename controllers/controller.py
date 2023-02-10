fa137535a139590ea2b519bea6ff687534030a14
SShingler <s.shingler@hotmail.co.uk>
2023-02-10 12:41:03 +0000

    2:Basic set up

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,3 +3,15 @@ from app import app
 from models.book import *
 from models.book_list import *
 from datetime import datetime
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/stock')
+def display_stock():
+    return render_template('display_books.html', book_list = book_list)
+
+@app.route('/stock/add')
+def add_stock():
+    return render_template('add_book.html')
--- models/book_list.py
+++ models/book_list.py
@@ -1,2 +1,8 @@
 from models.book import *
 import datetime
+
+book_1 = Book("Python for beginners", "Tim Wapling", "Education")
+book_2 = Book("HTML & CSS for beginners", "Barry Rodgers", "Education")
+book_3 = Book("JavaScript for beginners", "Mary Hollow", "Education")
+
+book_list = [book_1, book_2, book_3]
